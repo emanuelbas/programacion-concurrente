c7107f423add5b65b47ee5b15e97df2fc1617390
Emanuel <emanuelbastons@gmail.com>
2019-06-07 03:09:33 -0300

    .

--- Practica 4/08 - Tres impresoras un director.pas
+++ Practica 4/08 - Tres impresoras un director.pas
@@ -18,6 +18,40 @@ begin
 end;
 
 procedure coordinador
+
+begin
+	while ( true )
+		receive impresoras(i);
+		while ( true )
+			if ( !empty( cola_prioridad ) =>
+				receive cola_prioridad(documento)
+				send imprimir[i](documento);
+				break;
+			[] ( !empty( cola_normal ) and ( cola_prioridad'count = 0 ) ) =>
+				receive cola_normal(documento);
+				send imprimir[i](documento);
+				break;
+			end;
+		end;
+	end;
+end;
+
+procedure impresora [i:1..3]
+
+begin
+	while true
+		send impresoras(i);
+		receive imprimir[i](documento);
+		imprimir(documento);
+end;
+
+end.
+
+
+//Este coordinador es el primero que hice, tiene error
+//Ya que en ese instante donde se pasa del if al else podria llegar un pedido del director
+//La herramienta if no deterministico sirve para respetar la atomicidad por lo tanto es mejor usarla
+procedure coordinador
 var
 	boolean encontre_un_documento;
 begin
@@ -35,14 +69,3 @@ begin
 					send imprimir[i](documento);
 					encontre_un_documento := true;
 end;
-
-procedure impresora [i:1..3]
-
-begin
-	while true
-		send impresoras(i);
-		receive imprimir[i](documento);
-		imprimir(documento);
-end;
-
-end.
